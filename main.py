18000252bd29d8cfe462a11de842193c9cab0063
Mya <mya.ramsey@ufl.edu>
2024-04-10 23:27:42 -0400

    Renamed files to be easier to read. Got the files to read and be tokenized by the nltk library. Documentation on the google doc for processes.

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ import nltk
 
 def main():
     print("Hello World!")
+    with open('text/Knowledge-For-Humans.txt', 'r',encoding='utf-8') as file:
+        text = file.read()
+
+    sentences = nltk.sent_tokenize(text)
+    words = nltk.word_tokenize(text)
+
+    print(sentences)
+    print(words)
 
 
+main()
