075ba5f585a6749a4d750476cd0a6b95385ecc1e
JanelleW03 <yaayaa03j@gmail.com>
2024-04-19 17:41:28 -0400

    finishing the hashmap

--- main.py
+++ main.py
@@ -36,12 +36,13 @@ def get_important_names(text):
         print(f"{word}: {freq}")
 
 
-
 # used to change the string index into a useable key in the map
+# used code from geeksForGeeks: https://www.geeksforgeeks.org/hash-map-in-python/#
 def hash_function(s):
+    # uses separate chaining to deal with collsions
     hash_value = 0
     for char in s:
-        hash_value += ord(char)  # Adding ASCII value of each character
+        hash_value += ord(char)  # adds ASCII value of each character
     return hash_value
 class hashMap:
 
@@ -49,11 +50,13 @@ class hashMap:
     def __init__(self, size):
         self.size = size
         self.hash_table = self.create_buckets()
+        self.most_common = "";
+        self.common_count = 0;
 
     def create_buckets(self):
         return [[] for _ in range(self.size)]
 
-    def set_val(self, key, val):
+    def add_Value(self, key, count):
 
         # using the hash function to get the index
         hashed_key = hash_function(key) % self.size
@@ -65,20 +68,26 @@ class hashMap:
         for index, record in enumerate(bucket):
             record_key, record_val = record
 
-            # check if the bucket has same key as the key to be inserted
+        # check if the bucket has same key as the key to be inserted
             if record_key == key:
                 found_key = True
                 break
 
         # if the key to be inserted is in the bucket then increment the value
-        # else add the value with a count of 1
+        # else add to the value with a count of 1
         if found_key:
-            val += 1
-            bucket[index] = (key, val)
+            count += 1
+            bucket[index] = (key, count)
         else:
             bucket.append((key, 1))
 
+        # finding the most frequent word in the hash map
+        if count > self.common_count:
+            self.most_common = key
+            self.common_count = count
 
+    def print_most_common (map):
+        print(f"The word '{map.most_common}' shows up the most, with a count of {map.common_count}.")
 
 
 #Charles Code Start
@@ -116,7 +125,7 @@ class HashTable:
         t = sorted(self.hashTable)
         for i in t:
             print(f'{i.key}: {i.instances}')
-            
+
 #Charles Code End
 
 
