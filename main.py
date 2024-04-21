e087c253a8dd432e7016aed635f16aad368b95a4
charles-ja <128272836+charles-ja@users.noreply.github.com>
2024-04-21 16:31:00 -0400

    Update main.py
    charles changes

--- main.py
+++ main.py
@@ -102,29 +102,33 @@ class structSimulator:
     def __gt__(self, less):
         return self.instances < less.instances
 
-class HashTable:
+class MaxHeap:
     def __init__(self):
-        self.hashTable = set()
-        self.bucketValue = 0
+        self.maxHeap = []
 
     #For every duplicate found, increment by one
-    def insert(self, key):
-        keyFound = False
-        for iter in self.hashTable:
-            if key == iter.key:
-                iter.instances +=1
-                keyFound=True
-
-    #For every new word for an object will be made for it
-        if not keyFound:
-            y =structSimulator(key, 1)
-            self.hashTable.add(y)
+    def insert(self, key, frequency):
+        heap = structSimulator(key, frequency * -1)
+        self.maxHeap.append(heap)
+    #     keyFound = False
+    #     for iter in self.hashTable:
+    #         if key == iter.key:
+    #             iter.instances +=1
+    #             keyFound=True
+    #
+    # #For every new word for an object will be made for it
+    #     if not keyFound:
+    #         y =structSimulator(key, 1)
+    #         self.hashTable.add(y)
+    def Heapify(self):
+        heapq.heapify(self.maxHeap)
 
     #Will print out the duplicates in order of frequency
     def printInOrder(self):
-        t = sorted(self.hashTable)
-        for i in t:
-            print(f'{i.key}: {i.instances}')
+        t = self.maxHeap
+        while t.__sizeof__() != 0:
+            x = heapq.heappop(self.maxHeap)
+            print(f'{x.key}: {x.instances}')
 
 #Charles Code End
 
