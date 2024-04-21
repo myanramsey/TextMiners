3e9d9f4824cf857595eb2685e2c97ee7b427b1fe
Mya <mya.ramsey@ufl.edu>
2024-04-21 16:38:53 -0400

    - Organized code and fixed merge conflicts

--- main.py
+++ main.py
@@ -2,34 +2,6 @@ import nltk
 from nltk.corpus import stopwords
 from collections import Counter
 import re
-=======
-class MaxHeap:
-    def __init__(self):
-        self.maxHeap = []
-
-    #For every duplicate found, increment by one
-    def insert(self, key, frequency):
-        heap = structSimulator(key, frequency * -1)
-        self.maxHeap.append(heap)
-    #     keyFound = False
-    #     for iter in self.hashTable:
-    #         if key == iter.key:
-    #             iter.instances +=1
-    #             keyFound=True
-    #
-    # #For every new word for an object will be made for it
-    #     if not keyFound:
-    #         y =structSimulator(key, 1)
-    #         self.hashTable.add(y)
-    def Heapify(self):
-        heapq.heapify(self.maxHeap)
-
-    #Will print out the duplicates in order of frequency
-    def printInOrder(self):
-        t = self.maxHeap
-        while t.__sizeof__() != 0:
-            x = heapq.heappop(self.maxHeap)
-            print(f'{x.key}: {x.instances}')
 
 def get_important_names(text):
     # Get important Names
