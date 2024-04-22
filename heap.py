7090a2253d9a9c4ed5fcf88fdef21e5f375996e7
charles-ja <128272836+charles-ja@users.noreply.github.com>
2024-04-22 15:42:34 -0400

    Update heap.py
    charles change

--- heap.py
+++ heap.py
@@ -13,7 +13,7 @@ class structSimulator:
 class MaxHeap:
     def __init__(self):
         self.size = 0
-        self.maxHeap = []
+        self.max_heap = []
 
     #function used to heapify array
     def heapify(self, N, i):
@@ -24,7 +24,7 @@ class MaxHeap:
         # See if left child of root exists and is
         # less than root
         if r < self.size:
-            if self.maxHeap[l].instances < N and self.maxHeap[largest].instances < self.maxHeap[l].instances:
+            if self.max_heap[l].instances < N and self.max_heap[largest].instances < self.max_heap[l].instances:
                 largest = l
 
         # See if right child of root exists and is
@@ -32,12 +32,12 @@ class MaxHeap:
 
         #r needs to be less than size or else there will be an error
         if r < self.size:
-            if self.maxHeap[r].instances < N and self.maxHeap[largest].instances < self.maxHeap[r].instances:
+            if self.max_heap[r].instances < N and self.max_heap[largest].instances < self.max_heap[r].instances:
                 largest = r
 
         # Change root, if needed
         if largest != i:
-            self.maxHeap[i], self.maxHeap[largest] = self.maxHeap[largest], self.maxHeap[i]  # swap
+            self.max_heap[i], self.max_heap[largest] = self.max_heap[largest], self.max_heap[i]  # swap
 
             # Heapify the root.
             self.heapify(N, largest)
@@ -46,25 +46,25 @@ class MaxHeap:
     def insert(self, key):
         keyFound = False
         heap = structSimulator(key, 1)
-        for index, i in enumerate(self.maxHeap):
+        for index, i in enumerate(self.max_heap):
             if i.key == heap.key:
                 keyFound = True
-                self.maxHeap[index].instances +=1
-                self.heapify(self.maxHeap[index].instances, 0)
+                self.max_heap[index].instances +=1
+                self.heapify(self.max_heap[index].instances, 0)
 
         #if no duplicate is found, add new struct, and put new append at the front of list to heapify it
         if keyFound is False:
             self.size +=1
-            self.maxHeap.append(heap)
+            self.max_heap.append(heap)
             i = self.size -1
             while i > 0:
-                self.maxHeap[i], self.maxHeap[i -1] = self.maxHeap[i -1], self.maxHeap[i]  # swap
+                self.max_heap[i], self.max_heap[i - 1] = self.max_heap[i - 1], self.max_heap[i]  # swap
                 i-=1
             self.heapify(heap.instances, 0)
 
 
     #function utilized to sort heap
-    def heapSort(self):
+    def heap_sort(self):
         N = self.size
 
         # Build a maxheap.
@@ -73,20 +73,18 @@ class MaxHeap:
 
         # One by one extract elements
         for i in range(N - 1, 0, -1):
-            self.maxHeap[i], self.maxHeap[0] = self.maxHeap[0], self.maxHeap[i]  # swap
+            self.max_heap[i], self.max_heap[0] = self.max_heap[0], self.max_heap[i]  # swap
             self.heapify(i, 0)
 
     #Will print out the duplicates in order of frequency, top ten and least 10
-    def printInOrder(self):
-        self.heapSort()
+    def print_in_order(self):
+        self.heap_sort()
         print("10 Most Common Words:")
-        for i in self.maxHeap[len(self.maxHeap)-1:len(self.maxHeap)-11:-1]:
+        for i in self.max_heap[len(self.max_heap) - 1:len(self.max_heap) - 11:-1]:
             print(f'{i.key}: {i.instances}')
 
         print()
 
         print("10 Least Common Words:")
-        for i in self.maxHeap[0:10]:
+        for i in self.max_heap[0:10]:
             print(f'{i.key}: {i.instances}')
-
-
